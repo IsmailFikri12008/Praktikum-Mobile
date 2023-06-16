b734dcd420092ca4b68cd302a26d9bf12fbb4328
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2023-06-16 20:57:28 +0700

    menyelesaikan perbaikan

--- MobCrafter/app/src/main/java/id/ac/unpas/mobcrafter/screens/Beranda.kt
+++ MobCrafter/app/src/main/java/id/ac/unpas/mobcrafter/screens/Beranda.kt
@@ -19,10 +19,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import id.ac.unpas.mobcrafter.ui.theme.*
+import androidx.compose.ui.unit.*
 
 
 //@Composable
@@ -44,12 +46,19 @@ fun Beranda(
 ) {
     Box(
         modifier = Modifier
-            .background(BgHome)
             .fillMaxSize()
     ) {
         Column {
             GreetingSection()
             DaftarMenu(navController = navController)
+            Text(
+                text = "© 2023 MobCrafter",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 17.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+
         }
     }
 }
@@ -69,10 +78,16 @@ fun GreetingSection(
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Selamat Datang Di Aplikasi"
+                text = "Selamat Datang Di Aplikasi",
+                fontWeight = FontWeight.Bold,
+                fontSize = 29.sp,
+                modifier = Modifier.padding(bottom = 8.dp)
+
             )
             Text(
-                text = "Pengelolaan Mahasiswa"
+                text = "Pengelolaan Perkuliahan",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp
             )
         }
     }
@@ -105,7 +120,7 @@ fun DaftarMenu(
                 modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
             ) {
                 Column(
-                    modifier = Modifier.weight(0.7f)
+                    modifier = Modifier.weight(0.75f)
                 ) {
                     Text(
                         text = "Mahasiswa",
@@ -125,62 +140,16 @@ fun DaftarMenu(
                         navController.navigate("pengelolaan-mahasiswa")
                     },
                     colors = ColorsButton,
-                    modifier = Modifier.weight(0.3f).clip(RoundedCornerShape(25.dp))
+                    modifier = Modifier.weight(0.25f).clip(RoundedCornerShape(25.dp))
                 ) {
                     Icon(
                         imageVector = Icons.Default.PlayArrow,
                         contentDescription = "Icon",
-                        modifier = Modifier.padding(8.dp)
+                        modifier = Modifier.size(30.dp)
                     )
                 }
             }
         }
-
-        // Bagian Dosen
-        Box(
-            modifier = Modifier
-                .padding(vertical = 10.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(color)
-                .fillMaxWidth()
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
-            ) {
-                Column(
-                    modifier = Modifier.weight(0.7f)
-                ) {
-                    Text(
-                        text = "Dosen",
-                        fontWeight = FontWeight.SemiBold,
-                        color = text1,
-                        fontSize = 25.sp
-                    )
-                    Text(
-                        text = "Data Keseluruhan Dosen",
-                        fontWeight = FontWeight.Normal,
-                        color = text2,
-                        fontSize = 15.sp
-                    )
-                }
-                Button(
-                    onClick = {
-                        navController.navigate("pengelolaan-dosen")
-                    },
-                    colors = ColorsButton,
-                    modifier = Modifier.weight(0.3f).clip(RoundedCornerShape(25.dp))
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.PlayArrow,
-                        contentDescription = "Icon",
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
-            }
-        }
-
         // Bagian Matakuliah
         Box(
             modifier = Modifier
@@ -195,7 +164,7 @@ fun DaftarMenu(
                 modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
             ) {
                 Column(
-                    modifier = Modifier.weight(0.7f)
+                    modifier = Modifier.weight(0.75f)
                 ) {
                     Text(
                         text = "Matakuliah",
@@ -215,146 +184,59 @@ fun DaftarMenu(
                         navController.navigate("pengelolaan-matakuliah")
                     },
                     colors = ColorsButton,
-                    modifier = Modifier.weight(0.3f).clip(RoundedCornerShape(25.dp))
+                    modifier = Modifier.weight(0.25f).clip(RoundedCornerShape(25.dp))
                 ) {
                     Icon(
                         imageVector = Icons.Default.PlayArrow,
                         contentDescription = "Icon",
-                        modifier = Modifier.padding(8.dp)
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+            }
+        }
+        // Bagian Dosen
+        Box(
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .background(color)
+                .fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
+            ) {
+                Column(
+                    modifier = Modifier.weight(0.75f)
+                ) {
+                    Text(
+                        text = "Dosen",
+                        fontWeight = FontWeight.SemiBold,
+                        color = text1,
+                        fontSize = 25.sp
+                    )
+                    Text(
+                        text = "Data Keseluruhan Dosen",
+                        fontWeight = FontWeight.Normal,
+                        color = text2,
+                        fontSize = 15.sp
+                    )
+                }
+                Button(
+                    onClick = {
+                        navController.navigate("pengelolaan-dosen")
+                    },
+                    colors = ColorsButton,
+                    modifier = Modifier.weight(0.25f).clip(RoundedCornerShape(25.dp))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = "Icon",
+                        modifier = Modifier.size(30.dp)
                     )
                 }
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//@ExperimentalFoundationApi
-//@Composable
-//fun FeatureSection(features: List<Feature>) {
-//    Column(modifier = Modifier.fillMaxWidth()) {
-//        Text(
-//            text = "Features",
-//            style = MaterialTheme.typography.h1,
-//            modifier = Modifier.padding(15.dp)
-//        )
-//        LazyVerticalGrid(
-//            cells = GridCells.Fixed(2),
-//            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
-//            modifier = Modifier.fillMaxHeight()
-//        ) {
-//            items(features.size) {
-//                FeatureItem(feature = features[it])
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//fun FeatureItem(
-//    feature: Feature
-//) {
-//    BoxWithConstraints(
-//        modifier = Modifier
-//            .padding(7.5.dp)
-//            .aspectRatio(1f)
-//            .clip(RoundedCornerShape(10.dp))
-//            .background(feature.darkColor)
-//    ) {
-//        val width = constraints.maxWidth
-//        val height = constraints.maxHeight
-//
-//        // Medium colored path
-//        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
-//        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-//        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-//        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-//        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
-//
-//        val mediumColoredPath = Path().apply {
-//            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
-//            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
-//            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
-//            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
-//            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
-//            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-//            lineTo(-100f, height.toFloat() + 100f)
-//            close()
-//        }
-//
-//        // Light colored path
-//        val lightPoint1 = Offset(0f, height * 0.35f)
-//        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
-//        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
-//        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
-//        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
-//
-//        val lightColoredPath = Path().apply {
-//            moveTo(lightPoint1.x, lightPoint1.y)
-//            standardQuadFromTo(lightPoint1, lightPoint2)
-//            standardQuadFromTo(lightPoint2, lightPoint3)
-//            standardQuadFromTo(lightPoint3, lightPoint4)
-//            standardQuadFromTo(lightPoint4, lightPoint5)
-//            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-//            lineTo(-100f, height.toFloat() + 100f)
-//            close()
-//        }
-//        Canvas(
-//            modifier = Modifier
-//                .fillMaxSize()
-//        ) {
-//            drawPath(
-//                path = mediumColoredPath,
-//                color = feature.mediumColor
-//            )
-//            drawPath(
-//                path = lightColoredPath,
-//                color = feature.lightColor
-//            )
-//        }
-//        Box(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(15.dp)
-//        ) {
-//            Text(
-//                text = feature.title,
-//                style = MaterialTheme.typography.h2,
-//                lineHeight = 26.sp,
-//                modifier = Modifier.align(Alignment.TopStart)
-//            )
-//            Icon(
-//                painter = painterResource(id = feature.iconId),
-//                contentDescription = feature.title,
-//                tint = Color.White,
-//                modifier = Modifier.align(Alignment.BottomStart)
-//            )
-//            Text(
-//                text = "Start",
-//                color = TextWhite,
-//                fontSize = 14.sp,
-//                fontWeight = FontWeight.Bold,
-//                modifier = Modifier
-//                    .clickable {
-//                        // Handle the click
-//                    }
-//                    .align(Alignment.BottomEnd)
-//                    .clip(RoundedCornerShape(10.dp))
-//                    .background(ButtonBlue)
-//                    .padding(vertical = 6.dp, horizontal = 15.dp)
-//            )
-//        }
-//    }
-//}
